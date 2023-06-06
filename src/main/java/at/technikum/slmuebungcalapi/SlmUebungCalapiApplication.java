9706ff79b77ead5a251489cd662333e2f53ce583
WI22B027 <wi22b027@technikum-wien.at>
2023-06-06 18:45:53 +0200

    initialcommit

---
+++ src/main/java/at/technikum/slmuebungcalapi/SlmUebungCalapiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slmuebungcalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmUebungCalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmUebungCalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/slmuebungcalapi/SlmUebungCalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmuebungcalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmUebungCalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
